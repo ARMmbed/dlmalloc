b5647ff146121779165b136c217e0016518115e2
Brendan Moran <brendan.moran@arm.com>
2015-06-26 11:40:36 +0100

    Add configuration for dlmalloc.

--- source/dlmalloc.c
+++ source/dlmalloc.c
@@ -3,6 +3,42 @@
   Doug Lea and released to the public domain, as explained at
   http://creativecommons.org/publicdomain/zero/1.0/ Send questions,
   comments, complaints, performance data, etc to dl@cs.oswego.edu
+* Version 2.8.6 Wed Aug 29 06:57:58 2012  Doug Lea
+   Note: There may be an updated version of this malloc obtainable at
+           ftp://gee.cs.oswego.edu/pub/misc/malloc.c
+         Check before installing!
+ */
+
+
+/* configuration */
+#include <stdint.h>
+
+extern void *sbrk(intptr_t increment);
+
+#define LACKS_FCNTL_H
+#define LACKS_SYS_PARAM_H
+#define stderr 0
+#define HAVE_MORECORE 1
+#define MORECORE sbrk
+#define MORECORE_CONTIGUOUS 1
+#define MORECORE_CANNOT_TRIM 1
+#define USE_DL_PREFIX
+#define LACKS_TIME_H
+
+#define HAVE_MMAP 0
+#define HAVE_MREMAP 0
+
+#define NO_MALLINFO 1
+#define NO_MALLOC_STATS 1
+
+#define DEFAULT_GRANULARITY 64
+
+/* end configuration*/
+/*
+  This is a version (aka dlmalloc) of malloc/free/realloc written by
+  Doug Lea and released to the public domain, as explained at
+  http://creativecommons.org/publicdomain/zero/1.0/ Send questions,
+  comments, complaints, performance data, etc to dl@cs.oswego.edu
 
 * Version 2.8.6 Wed Aug 29 06:57:58 2012  Doug Lea
    Note: There may be an updated version of this malloc obtainable at
